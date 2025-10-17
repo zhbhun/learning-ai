4ccc2b83180090558abfac76bfd7d9103f25a7d3
zhbhun <zhbhun@gmail.com>
2025-10-17 23:02:31 +0800

    docs: langchain 使用 prompt

--- libraries/sdk/langchain/tester/hello.test.ts
+++ libraries/sdk/langchain/tester/hello.test.ts
@@ -1,16 +1,26 @@
 import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
 import { HumanMessage, SystemMessage } from '@langchain/core/messages'
+import { ChatPromptTemplate } from '@langchain/core/prompts'
 
 const model = new ChatGoogleGenerativeAI({
   model: 'gemini-2.0-flash-lite',
 })
 
-async function translate(content: string) {
-  const response = await model.invoke([
-    new SystemMessage('Translate the following from English into Chinese'),
-    new HumanMessage(content),
+async function translate(text: string, language = 'Chinese') {
+  const promptTemplate = ChatPromptTemplate.fromMessages([
+    ['system', 'Translate the following from English into {language}'],
+    ['user', '{text}'],
   ])
-  return response
+  const promptValue = await promptTemplate.invoke({
+    language,
+    text,
+  })
+  const promptMessages = promptValue.toChatMessages()
+  // [
+  //   new SystemMessage(`Translate the following from English into ${language}`),
+  //   new HumanMessage(text),
+  // ]
+  return model.invoke(promptMessages)
 }
 
 describe('translate function', () => {
@@ -18,6 +28,30 @@ describe('translate function', () => {
 
   test('should handle single word translation', async () => {
     const result = await translate('cat')
+    // const result: AIMessage = {
+    //   id: 'run-06f39944-31b7-47fa-9f14-71eefef7b275',
+    //   content: '猫 (māo)\n',
+    //   additional_kwargs: {
+    //     finishReason: 'STOP',
+    //     avgLogprobs: -0.005241605763634046,
+    //   },
+    //   response_metadata: {
+    //     tokenUsage: {
+    //       promptTokens: 8,
+    //       completionTokens: 6,
+    //       totalTokens: 14,
+    //     },
+    //     finishReason: 'STOP',
+    //     avgLogprobs: -0.005241605763634046,
+    //   },
+    //   tool_calls: [],
+    //   invalid_tool_calls: [],
+    //   usage_metadata: {
+    //     input_tokens: 8,
+    //     output_tokens: 6,
+    //     total_tokens: 14,
+    //   },
+    // }
     console.log(result)
     expect(result).toBeDefined()
     expect(result.content).toMatch(/猫/i)
